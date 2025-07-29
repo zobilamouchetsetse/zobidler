01753996c600754cdeabc5ff120a3b486e34e4cb
zobilamouchetsetse <y.bremand@gmail.com>
2025-07-29 23:01:05 +0200

    fix: ajout plugin react vite

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const App = () => (
+  <div style={{ backgroundColor: '#000', color: '#0ff', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+    <h1>SexIdler Clone - Dark Neon Style</h1>
+  </div>
+);
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
